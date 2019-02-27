1858f34c3e3a99a59ce13334c9277f93d898387f
katam8 <philliptam986@gmail.com>
2019-02-27 13:03:23 -0800

    Added Buttons

--- out/source/Minesweeper.java
+++ out/source/Minesweeper.java
@@ -1,5 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-import de.bezier.guido.*;
+import de.bezier.guido.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Minesweeper extends PApplet {
+
+
+
 //Declare and initialize NUM_ROWS and NUM_COLS = 20
 public final static int NUM_ROWS = 20;
 public final static int NUM_COLS = 20;
@@ -8,9 +26,9 @@ private ArrayList <MSButton> bombs = new ArrayList <MSButton>(); //ArrayList of 
 
 
 
-void setup ()
+public void setup ()
 {
-    size(400, 400);
+    
     textAlign(CENTER,CENTER);
     
     // make the manager
@@ -124,3 +142,13 @@ public class MSButton
 
 
 
+  public void settings() {  size(400, 400); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
